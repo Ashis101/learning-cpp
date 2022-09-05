75925e9b12f9d4da06bf20a9a188327c9ed24123
Ashis101 <ash627@protonmail.com>
2022-09-05 21:58:19 +0530

    how get values from user and append to string,char and int

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include<array>
 using namespace std;
 
 string i(string a){
@@ -8,10 +9,25 @@ string i(string a){
 }
 
 int main(){
-    int a=10, b=20;
-    string hi;
-    cin>> hi; 
-    cout << i(hi);
+    // int a=10, b=20;
+    // string hi;
+    // cin>> hi; 
+    // cout << i(hi);
+    // char a[5];
+    // for(int i=0;i<5;i++){
+    //     cin>>a[i];
+    // }
+    // cout<< a[1];
+
+    char ram[50];
+
+    cin>>ram;
+
+    cout<<"value of ram"<<" "<<ram;
+
+    int i=0;
+
+    
     // cout<<"hello world c"<<"::"<<a+b ;
     // cout<<"\n "<<"::"<<hi;
     return 0;
