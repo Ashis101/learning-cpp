bb34aad15f6ecec4447845b1fb9370122574650b
Ashis Banerjee <58388611+Ashis101@users.noreply.github.com>
2022-10-21 10:01:21 +0530

    worked on string in vector

--- stl-cpp/vector.cpp
+++ stl-cpp/vector.cpp
@@ -3,39 +3,26 @@
 
 using namespace std;
 
-void printVec(vector<int> v){
-    // using v as variable means coping all thing in v parameter ,coping things takes O(n) times which is costly 
-    // for optimization (as per situation) use reference like &v
-    
-    for (int i = 0; i < v.size(); i++)
-    {
-        // v.size() -> O(1)
-        cout<<v[i]<<" ";
+void printVec(vector<string> &v){
 
-
+    for(int i=0;i<v.size();i++){
+        cout<<v[i];
     }
-    
-
 }
 
 int main(){
-    // (define size , fill with this value) vector
-    // vector<int> v(10,false);
-    // initialize  vector
-    vector<int> v;
+    vector<string> v;
     // taking input from user
     int n;
     cin >> n;
     for (int i = 0; i < n; i++)
     {
-        int x ;
+        string x ;
         cin>> x;
         v.push_back(x);
     }
 
-    // coping vector to another vector
-    vector<int>v2=v; // O(n)
-    
+    // coping vector to another vector    
     printVec(v);
 
 }
