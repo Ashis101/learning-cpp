9ecb323a1ba6db767fe537c9cab63595e0db2985
Ashis Banerjee <58388611+Ashis101@users.noreply.github.com>
2022-10-21 10:44:20 +0530

    vector of vector

--- stl-cpp/nesting.cpp
+++ stl-cpp/nesting.cpp
@@ -6,19 +6,22 @@ using namespace std;
 
 int main(){
 
-    vector<pair<int,int>> v;
-    v={{1,2},{0,3},{0,3}};
+    // vector of array 
+    //vector<int> v[3]; // 3 is not point to size of vector it point to number of array in vector
+    // [3] means v[[],[],[]]
 
-    for(int i=0;i<v.size();i++){{
-        cout<<v[i].first<<endl;
+    vector<vector<int>> v;
 
-    }}
-
-    for(int i=0;i<v.size();i++){{
-        cout<<v[i].second<<endl;
-
-    }}
-
+    for(int i=0;i<3;i++){
+        int x;
+        cin>>x;
+        vector<int> temp;
+        for(int j;j<x;j++){
+            int y;
+            temp.push_back(y);
+        }
+        v.push_back(temp);
+    }
 
 
 }
