de899970929bb3126568e1752cfa68106f4e58aa
Ashis Banerjee <58388611+Ashis101@users.noreply.github.com>
2022-10-21 10:17:44 +0530

    vector of pair

---
+++ stl-cpp/nesting.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+
+    vector<pair<int,int>> v;
+    v={{1,2},{0,3},{0,3}};
+
+    for(int i=0;i<v.size();i++){{
+        cout<<v[i].first<<endl;
+
+    }}
+
+    for(int i=0;i<v.size();i++){{
+        cout<<v[i].second<<endl;
+
+    }}
+
+
+
+}
+
